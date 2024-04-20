20293d265db77988239336bc445142a7a03c2082
randomman552 <random.man552@protonmail.com>
2024-04-20 21:26:32 +0100

    Add basic cli setup

--- go.mod
+++ go.mod
@@ -1,3 +1,12 @@
 module jellyfin-exporter
 
 go 1.21.5
+
+require github.com/urfave/cli/v2 v2.27.1
+
+require (
+	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
+	github.com/joho/godotenv v1.5.1
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
+)
--- main.go
+++ main.go
@@ -1,5 +1,42 @@
 package main
 
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/urfave/cli/v2"
+)
+
 func main() {
+	godotenv.Load()
+
+	app := cli.NewApp()
+	app.Name = "Jellyfin Prometheus Exporter"
+	app.Usage = "Prometheus metrics exporter for Jellfin"
+	app.Action = run
+
+	app.Flags = []cli.Flag{
+		&cli.StringFlag{
+			Name: "jellyfin.url",
+			EnvVars: []string{
+				"JELLYFIN_URL",
+			},
+		},
+		&cli.StringFlag{
+			Name: "jellyfin.api_key",
+			EnvVars: []string{
+				"JELLYFIN_API_KEY",
+			},
+		},
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func run(c *cli.Context) error {
 	print("Hello World!")
+	return nil
 }
