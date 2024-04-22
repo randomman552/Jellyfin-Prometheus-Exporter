a4d816e5a74f3129609a7f2dd823f2f6c5c8320c
randomman552 <random.man552@protonmail.com>
2024-04-22 20:08:39 +0100

    Add basic library stats

--- collectors/library.go
+++ collectors/library.go
@@ -4,15 +4,27 @@ import (
 	"jellyfin-exporter/api"
 
 	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
 )
 
 type LibraryCollector struct {
 	Client api.JellyfinClient
+
+	Libraries *prometheus.GaugeVec
 }
 
 func NewLibraryCollector(client *api.JellyfinClient) *LibraryCollector {
 	return &LibraryCollector{
 		Client: *client,
+
+		Libraries: promauto.NewGaugeVec(prometheus.GaugeOpts{
+			Name: "jellyfin_library_count",
+			Help: "Number of items in each Jellyfin library",
+		}, []string{
+			"library",
+			"libraryType",
+			"itemType",
+		}),
 	}
 }
 
@@ -22,5 +34,20 @@ func (c *LibraryCollector) Describe(ch chan<- *prometheus.Desc) {
 
 // Collect metrics from sessions returned from the Jellyfin API
 func (c *LibraryCollector) Collect(metrics chan<- prometheus.Metric) {
+	virtualFolders := c.Client.GetVirtualFolders()
 
+	for _, folder := range virtualFolders {
+		// Get items
+		itemResponse := c.Client.GetItems(folder.ItemId)
+
+		// Group items by their type
+		// This is because a library can contain series, which can contain seasons, which can contain episodes...
+		groupedItems := GroupByProperty(itemResponse.Items, func(item api.JellyfinItem) string {
+			return item.Type
+		})
+
+		for key, items := range groupedItems {
+			c.Libraries.WithLabelValues(folder.Name, folder.CollectionType, key).Set(float64(len(items)))
+		}
+	}
 }
